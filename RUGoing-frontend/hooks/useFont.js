1b38125e83186c097db177d657cbf718cd9bbfad
Vikram Sridhar <mmarkiv7@gmail.com>
2023-10-18 16:34:41 -0400

    cleaned up folder structure

--- RUGoing-frontend/hooks/useFont.js
+++
@@ -1,8 +0,0 @@
-import * as Font from "expo-font";
-
-export default useFonts = async () =>
-  await Font.loadAsync({
-    "Poppins-Regular": require("../assets/fonts/Poppins-Regular.ttf"),
-    "Poppins-Medium": require("../assets/fonts/Poppins-Medium.ttf"),
-    "Poppins-Thin": require("../assets/fonts/Poppins-Thin.ttf"),
-  });
