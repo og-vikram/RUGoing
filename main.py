56d0c08dae4146654c2264ec59bdb4ff732979b5
mahfuzarahman <rmahfuza0@gmail.com>
2023-11-13 16:45:50 -0500

    more routes

--- main.py
+++ main.py
@@ -20,9 +20,11 @@ cursor = conn.cursor()
 try:
     # events.get_event_details(cursor, '9569310')
     # events.get_all_events(cursor)
-    events.modify_dates(cursor)
+    # events.modify_dates(cursor)
     # organizations.get_all_organizations(cursor)
     # organizations.get_categories(conn, cursor)
+    # events.get_categories(conn, cursor)
+    events.get_themes(conn, cursor)
     # cursor.execute("SELECT * FROM Organizations")
     # rows = cursor.fetchall()  # Fetch all rows
     # for row in rows:
--- scraping/events.py
+++ scraping/events.py
@@ -186,7 +186,7 @@ def get_categories(conn, cursor):
     try:
         categories_length = len(chrome.find_element(By.XPATH, categories_xpath).find_elements(By.XPATH, './child::*'))
         time.sleep(0.1)
-        for i in range(categories_length):
+        for i in range(10, categories_length):
             WebDriverWait(chrome, 10).until(
                 EC.visibility_of_element_located(
                     (
@@ -262,20 +262,25 @@ def get_event_categories(cursor, category_id):
         event_list_container = soup.find_all("div", id="event-discovery-list")
         event_list = event_list_container[0].find_all("a")
             
-        for org in event_list:
-            event_id = str(org['href']).split('/')[3]
+        for event in event_list:
+            event_id = str(event['href']).split('/')[3]
+            print(event_id, category_id)
             
-            query = """INSERT INTO CategorizedEvents (org_id, category_id)
-                       VALUES (%s, %s)
-                       ON DUPLICATE KEY UPDATE
-                       event_id = VALUES(event_id),
-                       category_id = VALUES(category_id);"""
-            cursor.execute(query, (event_id, category_id))
+            # query = """INSERT INTO CategorizedEvents (event_id, category_id)
+            #            VALUES (%s, %s)
+            #            ON DUPLICATE KEY UPDATE
+            #            event_id = VALUES(event_id),
+            #            category_id = VALUES(category_id);"""
+            query = """INSERT IGNORE INTO CategorizedEvents (event_id, category_id)
+                       SELECT %s, %s
+                       FROM Events
+                       WHERE event_id = %s"""
+            cursor.execute(query, (event_id, category_id, event_id))
             
     except NoSuchElementException as e:
         event_id = None
     
-def get_perks():
+def get_perks(conn, cursor):
     url = 'https://rutgers.campuslabs.com/engage/events?perks='
     chrome.get(url)
 
@@ -298,61 +303,192 @@ def get_perks():
             
             perks = chrome.find_element(By.XPATH, perks_xpath)
 
-            WebDriverWait(categories, 10).until(
+            WebDriverWait(perks, 10).until(
                 EC.visibility_of_all_elements_located((By.XPATH, './child::*'))
             )
             
-            category = categories.find_elements(By.XPATH, './child::*')[i]
-            name = category.text
-            category.click()
-            category_url = chrome.current_url
-            category_id = str(category_url).split('=')[1]
+            perk = perks.find_elements(By.XPATH, './child::*')[i]
+            name = perk.text
+            perk.click()
+            perk_url = chrome.current_url
+            perk_id = str(perk_url).split('=')[1]
             
-            query = """INSERT INTO EventCategories (category_id, name)
+            query = """INSERT INTO EventPerks (perk_id, name)
                        VALUES (%s, %s)
                        ON DUPLICATE KEY UPDATE
                        name = VALUES(name);"""
                        
-            cursor.execute(query, (category_id, name))
+            cursor.execute(query, (perk_id, name))
             conn.commit()
             
-            get_event_categories(cursor, category_id)
+            get_event_perks(cursor, perk_id)
 
             time.sleep(0.1)
             chrome.get(url)
             time.sleep(0.1)
-            categories_list = chrome.find_element(By.ID, "categories")
-            categories_list.click()
+            perks_list = chrome.find_element(By.ID, "perks")
+            perks_list.click()
     except NoSuchElementException as e:
-        categories = None
+        perks = None
     
-def get_event_perks():
-    url = 'https://rutgers.campuslabs.com/engage/events'
-    chrome.get(url)
+def get_event_perks(cursor, perk_id):
+    base_url = 'https://rutgers.campuslabs.com/engage/events?perks='
+    perk_url = base_url + perk_id
+    chrome.get(perk_url)
+        
+    try:
+        chrome.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        load_more_xpath = '/html/body/div[2]/div/div/div/div/div[4]/div/div/div[2]/div[2]/div[2]/button/div/div'
 
-    perks_list = chrome.find_element(By.ID, "perks")
-    perks_list.click()
-   
-    perks_xpath = '/html/body/div[4]/div[3]/ul'
-    perks = chrome.find_element(By.XPATH, perks_xpath).find_elements(By.XPATH, './child::*')
-    
-    time.sleep(3)
-    for perk in perks:
-        print(perk.text)
-    
-def get_event_themes():
-    url = 'https://rutgers.campuslabs.com/engage/events'
+        while True:
+            try:
+                load_more = chrome.find_element(By.XPATH, load_more_xpath, )
+                time.sleep(0.1)
+                load_more.click()
+                # check if element is visible -- this will throw TimeoutException if not
+                WebDriverWait(chrome, 10).until(
+                    EC.visibility_of_element_located(
+                        (
+                            By.XPATH,
+                            load_more_xpath,
+                        )
+                    )
+                )
+            except StaleElementReferenceException:
+                break
+            except TimeoutException:
+                break
+            except NoSuchElementException:
+                break
+        
+        html = chrome.page_source
+        soup = BeautifulSoup(html, "lxml")
+        time.sleep(3)
+        event_list_container = soup.find_all("div", id="event-discovery-list")
+        event_list = event_list_container[0].find_all("a")
+            
+        for event in event_list:
+            event_id = str(event['href']).split('/')[3]
+            
+            # query = """INSERT INTO PerkedEvents (event_id, perk_id)
+            #            VALUES (%s, %s)
+            #            ON DUPLICATE KEY UPDATE
+            #            event_id = VALUES(event_id),
+            #            perk_id = VALUES(perk_id);"""
+            
+            query = """INSERT IGNORE INTO PerkedEvents (event_id, perk_id)
+                       SELECT %s, %s
+                       FROM Events
+                       WHERE event_id = %s"""
+            cursor.execute(query, (event_id, perk_id, event_id))
+            
+    except NoSuchElementException as e:
+        event_id = None
+        
+def get_themes(conn, cursor):
+    url = 'https://rutgers.campuslabs.com/engage/events?themes='
     chrome.get(url)
 
     themes_list = chrome.find_element(By.ID, "themes")
     themes_list.click()
-   
-    themes_xpath = '/html/body/div[4]/div[3]/ul'
-    themes = chrome.find_element(By.XPATH, themes_xpath).find_elements(By.XPATH, './child::*')
     
-    time.sleep(3)
-    for theme in themes:
-        print(theme.text)
+    themes_xpath = '/html/body/div[4]/div[3]/ul'
+    try:
+        themes_length = len(chrome.find_element(By.XPATH, themes_xpath).find_elements(By.XPATH, './child::*'))
+        time.sleep(0.1)
+        for i in range(themes_length):
+            WebDriverWait(chrome, 10).until(
+                EC.visibility_of_element_located(
+                    (
+                        By.XPATH,
+                        themes_xpath,
+                    )
+                )
+            )
+            
+            themes = chrome.find_element(By.XPATH, themes_xpath)
+
+            WebDriverWait(themes, 10).until(
+                EC.visibility_of_all_elements_located((By.XPATH, './child::*'))
+            )
+            
+            theme = themes.find_elements(By.XPATH, './child::*')[i]
+            name = theme.text
+            theme.click()
+            theme_url = chrome.current_url
+            theme_id = str(theme_url).split('=')[1]
+            
+            query = """INSERT INTO EventThemes (theme_id, name)
+                       VALUES (%s, %s)
+                       ON DUPLICATE KEY UPDATE
+                       name = VALUES(name);"""
+                       
+            cursor.execute(query, (theme_id, name))
+            conn.commit()
+            
+            get_event_themes(cursor, theme_id)
+
+            time.sleep(0.1)
+            chrome.get(url)
+            time.sleep(0.1)
+            themes_list = chrome.find_element(By.ID, "themes")
+            themes_list.click()
+    except NoSuchElementException as e:
+        themes = None
+    
+def get_event_themes(cursor, theme_id):
+    base_url = 'https://rutgers.campuslabs.com/engage/events?themes='
+    theme_url = base_url + theme_id
+    chrome.get(theme_url)
+        
+    try:
+        chrome.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+        load_more_xpath = '/html/body/div[2]/div/div/div/div/div[4]/div/div/div[2]/div[2]/div[2]/button/div/div'
+
+        while True:
+            try:
+                load_more = chrome.find_element(By.XPATH, load_more_xpath, )
+                time.sleep(0.1)
+                load_more.click()
+                # check if element is visible -- this will throw TimeoutException if not
+                WebDriverWait(chrome, 10).until(
+                    EC.visibility_of_element_located(
+                        (
+                            By.XPATH,
+                            load_more_xpath,
+                        )
+                    )
+                )
+            except StaleElementReferenceException:
+                break
+            except TimeoutException:
+                break
+            except NoSuchElementException:
+                break
+        
+        html = chrome.page_source
+        soup = BeautifulSoup(html, "lxml")
+        time.sleep(3)
+        event_list_container = soup.find_all("div", id="event-discovery-list")
+        event_list = event_list_container[0].find_all("a")
+            
+        for event in event_list:
+            event_id = str(event['href']).split('/')[3]
+            
+            # query = """INSERT INTO ThemedEvents (event_id, theme_id)
+            #            VALUES (%s, %s)
+            #            ON DUPLICATE KEY UPDATE
+            #            event_id = VALUES(event_id),
+            #            theme_id = VALUES(theme_id);"""
+            
+            query = """INSERT IGNORE INTO ThemedEvents (event_id, theme_id)
+                       SELECT %s, %s
+                       FROM Events
+                       WHERE event_id = %s"""
+            cursor.execute(query, (event_id, theme_id, event_id))
+            
+    except NoSuchElementException as e:
+        event_id = None
         
 def modify_dates(cursor):
     query = """SELECT event_id FROM Events;"""
@@ -423,7 +559,7 @@ def modify_dates(cursor):
 
 # get_event_themes()
 # get_event_categories()
-get_event_perks()
+# get_event_perks()
 
 # get_event_details('https://rutgers.campuslabs.com/engage/event/9551789')
 # get_event_details('9509343')
