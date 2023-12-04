0333aab0f7253f1934e5f6ffe14a9943781474d0
Vikram Sridhar <mmarkiv7@gmail.com>
2023-12-04 17:46:08 -0500

    added home card files

---
+++ frontend/components/HomeEventCard.jsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const HomeEventCard = () => {
+  return (
+    <View>
+      <Text>HomeEventCard</Text>
+    </View>
+  );
+};
+
+export default HomeEventCard;
+
+const styles = StyleSheet.create({});
