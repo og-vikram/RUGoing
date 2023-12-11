6bc0222b5c9df1968bca93ad8e452aa9b3e8b273
jakef2001 <ajf269@rutgers.edu>
2023-12-11 18:59:06 -0500

    fix up eventprofilescreen

--- frontend/views/EventProfileScreen.jsx
+++ frontend/views/EventProfileScreen.jsx
@@ -10,25 +10,28 @@ import React, { useEffect, useLayoutEffect, useState } from "react";
 import { useNavigation, useRoute } from "@react-navigation/native";
 import { auth } from "../firebase.config";
 
+//EventProfileScreen component
 const EventProfileScreen = () => {
   const navigation = useNavigation();
   const route = useRoute();
+  //Extracting event ID from route parameters
   const eventId = route.params.eventId
     ? route.params.eventId
     : route.params.selectedProps.eventId;
+  //State variables
   const [data, setData] = useState([]);
   const [host, setHost] = useState([]);
   const [attending, setAttending] = useState(false);
   const [attendees, setAttendees] = useState([]);
 
-
+  //Fetch event data on component mount
   useLayoutEffect(() => {
     fetch(`https://absolute-willing-salmon.ngrok-free.app/api/event/${eventId}`)
       .then((response) => response.json())
       .then((json) => setData(json.event))
       .catch((error) => console.log(error));
   }, []);
-
+  //Fetch host information on component mount
   useLayoutEffect(() => {
     fetch(
       `https://absolute-willing-salmon.ngrok-free.app/api/event/host/${eventId}`
@@ -37,7 +40,7 @@ const EventProfileScreen = () => {
       .then((json) => setHost(json))
       .catch((error) => console.log(error));
   }, []);
-
+  //Fetch list of attendees on component mount
   useEffect(() => {
     fetch(
       `https://absolute-willing-salmon.ngrok-free.app/api/event/attending/${eventId}`
@@ -48,11 +51,11 @@ const EventProfileScreen = () => {
       })
       .catch((error) => console.log(error));
   }, []);
-
+  //Check if the user is attending the event on component mount
   useEffect(() => {
     fetch(
       "https://absolute-willing-salmon.ngrok-free.app/api/event/attending/" +
-        auth.currentUser.uid
+      auth.currentUser.uid
     )
       .then((response) => response.json())
       .then((json) => {
@@ -65,12 +68,13 @@ const EventProfileScreen = () => {
       .catch((error) => console.log(error));
   }, []);
 
+  //Function to handle attending an event
   const handleAttend = async () => {
     {
       OpenURLButton();
       try {
         const user = auth.currentUser;
-
+        //API call to add user to the list of event attendees
         fetch(
           `https://absolute-willing-salmon.ngrok-free.app/api/event/attending/add/`,
           {
@@ -90,13 +94,13 @@ const EventProfileScreen = () => {
       }
     }
   };
-
+  //Function to handle leaving an event
   const handleRemove = async () => {
     {
       OpenURLButton();
       try {
         const user = auth.currentUser;
-
+        //API call to remove user from the list of event attendees
         fetch(
           `https://absolute-willing-salmon.ngrok-free.app/api/event/attending/remove/`,
           {
@@ -116,20 +120,20 @@ const EventProfileScreen = () => {
       }
     }
   };
-
+  //Function to display the count of people attending the event
   const peopleCount = () => {
     if (attendees.length == 1) {
       return attendees.length + " person attending";
     }
     return attendees.length + " people attending";
   };
-
+  //Function to open the event's RSVP link in a browser
   const OpenURLButton = async () => {
     const supported = await Linking.canOpenURL(data.rsvp);
-    if(supported)
-    await Linking.openURL(data.rsvp);
+    if (supported)
+      await Linking.openURL(data.rsvp);
   }
-
+  //Render the component
   return (
     <ScrollView>
       <View className="container" style={styles.container}>
@@ -188,7 +192,7 @@ const EventProfileScreen = () => {
 };
 
 export default EventProfileScreen;
-
+//Component styling
 const styles = StyleSheet.create({
   modalContainer: {
     flex: 1,
